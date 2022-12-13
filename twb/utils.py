58b9aecc8397531eab761e9b719b7ae350150bfb
Hykilpikonna <me@hydev.org>
2022-12-13 14:18:45 -0500

    [+] Utils

---
+++ twb/utils.py
@@ -0,0 +1,14 @@
+import inspect
+
+from .constants import DEBUG
+
+
+def debug(msg: object) -> None:
+    """
+    Output a debug message, usually from another function
+
+    :param msg: Message
+    """
+    if DEBUG:
+        caller = inspect.stack()[1].function
+        print(f'[DEBUG] {caller}: {msg}')
