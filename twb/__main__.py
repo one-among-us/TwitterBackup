2ae92fcf1cba93b1490ac23244bfd48ab5344c74
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-08-03 12:30:13 +0800

    [U] Update launcher

--- twb/__main__.py
+++ twb/__main__.py
@@ -5,31 +5,33 @@ from pathlib import Path
 
 from hypy_utils import printc
 
-from .collect import tweepy_login, download_all_tweets, download_media
-from .config import load_config
+from twb.twitter_gql import TwitterGQL
+from .collect import download_media
 
 
 def run():
     parser = argparse.ArgumentParser("Twitter Backup Tool")
     parser.add_argument("username", help="@user of the user you want to back up")
     parser.add_argument("-p", "--path", help="Output path")
-    parser.add_argument("-c", "--config", help="Config file path")
+    parser.add_argument("-c", "--cookies", help="EditThisCookie export json file path", default="cookies.json")
     parser.add_argument("-m", "--multithread", help="Use multithreading when downloading media (breaks progress bar)",
                         action='store_true')
     args = parser.parse_args()
 
-    # Load config
-    config = load_config(args.config or "config.toml")
+    # Create API
+    cp = Path(args.cookies)
+    if not cp.exists():
+        printc('&cCookies file not found')
+        return
 
-    # Login
-    api = tweepy_login(config)
+    api = TwitterGQL(cp.read_text('utf-8'))
 
     # Convert path
-    BASEDIR = Path(args.path or "backups")
-    json_path = BASEDIR / args.username / 'tweets.json'
+    basedir = Path(args.path or "backups")
+    json_path = basedir / args.username / 'tweets.json'
 
     # Crawl tweets
-    download_all_tweets(api, args.username, json_path)
+    api.crawl_all(args.username)
 
     # Download media
     download_media(json_path, mt=args.multithread)
