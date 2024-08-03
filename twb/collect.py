feee4a47e3ddbb776573014012faad2de818f68e
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-08-03 13:41:38 +0800

    [+] Download media v2

--- twb/collect.py
+++ twb/collect.py
@@ -10,7 +10,9 @@ from hypy_utils.tqdm_utils import tmap
 
 def download_media(json_path: Path, mt: bool = False):
     """
-    Download media urls embedded in a twitter json to somewhere local
+    Download media URLs embedded in a Twitter JSON to somewhere local (v2)
+
+    v2: Recursively search for "media_url_https" in the entire JSON tree instead of just the top level and just entities
 
     :param json_path: Path to tweets.json
     :param mt: Multithread (might break progress bar display)
@@ -18,28 +20,30 @@ def download_media(json_path: Path, mt: bool = False):
     dp = json_path.parent
     obj = json.loads(json_path.read_text())
 
-    def download(media: dict):
-        url: str = media['media_url_https']
+    def download(url: str):
         fp = ensure_dir(dp / 'media') / url.split("/")[-1]
+        if fp.is_file():
+            return
         hypy_utils.downloader.download_file(url, fp)
-        media['local_media_path'] = str(fp.relative_to(dp))
 
-        # For videos
-        if media['type'] == 'video':
-            # Find video URL of the video with the highest bitrate
-            v = max(media['video_info']['variants'], key=lambda x: x.get('bitrate') or 0)
-            url = v['url']
-            fp = ensure_dir(dp / 'media' / 'video') / url.split("/")[-1].split("?")[0]
-            if not fp.is_file():
-                print(f"Downloading video {v}")
-                hypy_utils.downloader.download_file(url, fp)
-            media['local_video_path'] = str(fp.relative_to(dp))
+    def extract_media_urls(o: list | dict, target_key) -> list:
+        if isinstance(o, dict):
+            for k, v in o.items():
+                if k == target_key:
+                    yield o[target_key]
+                else:
+                    yield from extract_media_urls(v, target_key)
+        elif isinstance(o, list):
+            for item in o:
+                yield from extract_media_urls(item, target_key)
 
     # Gather media objects
-    medias = []
-    for t in obj:
-        medias += (t.get("entities") or {}).get("media") or []
-        medias += (t.get("extended_entities") or {}).get("media") or []
+    medias = list(extract_media_urls(obj, 'media_url_https'))
+
+    # Gather video objects
+    videos = list(extract_media_urls(obj, 'video_info'))
+    videos = [max(v['variants'], key=lambda x: x.get('bitrate') or 0)['url'] for v in videos]
+    medias += videos
 
     # Download
     if mt:
@@ -47,4 +51,6 @@ def download_media(json_path: Path, mt: bool = False):
     else:
         [download(m) for m in medias]
 
-    write(json_path, json_stringify(obj, indent=2))
+
+if __name__ == '__main__':
+    download_media(Path('backups/zzj876/tweets.json'))
