9b8e2b75ea6b9d79bb0ab7b7c861cf9cbce4af61
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-10-25 17:54:41 -0400

    [+] Tweet detail api

--- twb/twitter_gql.py
+++ twb/twitter_gql.py
@@ -3,17 +3,24 @@ import time
 from collections import Counter
 from pathlib import Path
 
+import orjson
 import requests
-from hypy_utils import ensure_parent, write_json
+from hypy_utils import ensure_parent, write_json, ensure_dir
 from hypy_utils.logging_utils import setup_logger
+from hypy_utils.tqdm_utils import tmap
+
+from twb.gql_consts import TWEETS_F
 
 log = setup_logger()
+cache_dir = ensure_dir(Path(__file__).parent / '.cache')
 
 
 class TwitterGQL:
     HTTP = requests.session()
 
-    def __init__(self, cookies: str) -> None:
+    def __init__(self, cookies: str, base_dir: Path = Path('backups')):
+        self.base_dir = Path(base_dir)
+
         # Parse json cookie
         bacon: list[dict] = json.loads(cookies)
         bacon: dict = {d['name']: d['value'] for d in bacon}
@@ -36,8 +43,8 @@ class TwitterGQL:
              "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
              "responsive_web_graphql_timeline_navigation_enabled": True}
         t = {"withAuxiliaryUserLabels": False}
-
-        return self._request('https://x.com/i/api/graphql/xmU6X_CKVnQ5lSrCbAmJsg/UserByScreenName', v, f, t)['data']['user']['result']
+        r = self._request('https://x.com/i/api/graphql/xmU6X_CKVnQ5lSrCbAmJsg/UserByScreenName', v, f, t)
+        return r['data']['user']['result']
 
     def user_tweets(self, user_id: int, cursor: str | None = None) -> (list, str, str):
         """
@@ -47,21 +54,7 @@ class TwitterGQL:
              "withVoice": True, "withV2Timeline": True}
         if cursor:
             v['cursor'] = cursor
-        f = {"rweb_tipjar_consumption_enabled": True, "responsive_web_graphql_exclude_directive_enabled": True,
-             "verified_phone_label_enabled": False, "creator_subscriptions_tweet_preview_api_enabled": True,
-             "responsive_web_graphql_timeline_navigation_enabled": True,
-             "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
-             "communities_web_enable_tweet_community_results_fetch": True,
-             "c9s_tweet_anatomy_moderator_badge_enabled": True, "articles_preview_enabled": True,
-             "responsive_web_edit_tweet_api_enabled": True,
-             "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
-             "view_counts_everywhere_api_enabled": True, "longform_notetweets_consumption_enabled": True,
-             "responsive_web_twitter_article_tweet_consumption_enabled": True,
-             "tweet_awards_web_tipping_enabled": False, "creator_subscriptions_quote_tweet_preview_enabled": False,
-             "freedom_of_speech_not_reach_fetch_enabled": True, "standardized_nudges_misinfo": True,
-             "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
-             "rweb_video_timestamps_enabled": True, "longform_notetweets_rich_text_read_enabled": True,
-             "longform_notetweets_inline_media_enabled": True, "responsive_web_enhance_cards_enabled": False}
+        f = TWEETS_F
         t = {"withArticlePlainText": False}
 
         inst: dict = self._request('https://x.com/i/api/graphql/bt4TKuFz4T7Ckk-VvQVSow/UserTweetsAndReplies', v, f, t)
@@ -87,6 +80,33 @@ class TwitterGQL:
 
         return tweets, cursors['Top'], cursors['Bottom']
 
+    def tweet_detail(self, tweet_id: str | int):
+        fp = cache_dir / f'tweet_detail/{tweet_id}.json'
+        if fp.exists():
+            r = orjson.loads(fp.read_bytes())
+        else:
+            v = {"focalTweetId": str(tweet_id), "referrer": "tweet", "with_rux_injections": False,
+                 "rankingMode": "Relevance", "includePromotedContent": True, "withCommunity": True,
+                 "withQuickPromoteEligibilityTweetFields": True, "withBirdwatchNotes": True, "withVoice": True}
+            f = TWEETS_F
+            t = {"withArticleRichContentState": True, "withArticlePlainText": False, "withGrokAnalyze": False,
+                 "withDisallowedReplyControls": False}
+            r: dict = self._request('https://x.com/i/api/graphql/nBS-WpgA6ZG0CyNHD517JQ/TweetDetail', v, f, t)
+            time.sleep(10)
+            write_json(fp, r)
+        if r.get('errors'):
+            log.error(f'Error: {r["errors"]}')
+            return []
+        r: list = r['data']['threaded_conversation_with_injections_v2']['instructions']
+
+        # Parse
+        r: list = [inst for inst in r if inst['type'] == 'TimelineAddEntries']
+        assert len(r) <= 1
+        if not r:
+            log.debug(f'No TimelineAddEntries for {tweet_id}')
+            return []
+        r: list = r[0]['entries']
+        return r
     def crawl_all(self, screen_name: str, rate_delay: float = 10) -> None:
         """
         Crawl all tweets of a user
